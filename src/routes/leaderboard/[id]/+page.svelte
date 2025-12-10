e00662965334507b58b92ab7de2837840244e76d
iphone <iphone@example.com>
2025-12-10 22:32:59 +0700

    feat: add clear filter button to the leaderboard filters

--- src/routes/leaderboard/[id]/+page.svelte
+++ src/routes/leaderboard/[id]/+page.svelte
@@ -858,6 +858,23 @@
                                         <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
                                     </div>
                                 </div>
+
+                                <!-- Clear Filter Button -->
+                                {#if filterSymbol !== 'ALL' || filterType !== 'ALL' || filterOutcome !== 'ALL'}
+                                    <button 
+                                        on:click={() => {
+                                            filterSymbol = 'ALL';
+                                            filterType = 'ALL';
+                                            filterOutcome = 'ALL';
+                                        }}
+                                        class="p-1.5 text-gray-400 hover:text-red-500 hover:bg-red-50 dark:hover:bg-red-900/20 rounded-full transition-colors"
+                                        title="Clear Filters"
+                                    >
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+                                        </svg>
+                                    </button>
+                                {/if}
                             </div>
                         </div>
                         <div class="overflow-x-auto">
